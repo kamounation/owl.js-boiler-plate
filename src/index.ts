190a5e5759b966f6918a2be54542cb5c0ab6b564
Xlaez <utibeabasiekong2@gmail.com>
2023-01-03 23:16:22 +0100

    update: checked current version of owl-factory and it works correctly

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { ENV, PORT } from "configs";
-import mongoConfig from "configs/db";
-import helmet from "helmet";
-import OwlFactory from "owl-factory";
-import ExamleRoute from "routes/example.routes";
+import { ENV, PORT } from 'configs';
+import mongoConfig from 'configs/db';
+import helmet from 'helmet';
+import OwlFactory from 'owl-factory';
+import ExamleRoute from 'routes/example.routes';
 
-const server = new OwlFactory([new ExamleRoute()], process.env.PORT || PORT, ENV, {mongodbConfig: mongoConfig, otherDbConfig: null});
-
+const server = new OwlFactory([new ExamleRoute()], process.env.PORT || PORT, ENV, {
+  mongodbConfig: mongoConfig,
+  otherDbConfig: null,
+});
 
 server.app.use(helmet());
 
